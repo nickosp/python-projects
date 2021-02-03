5be13728c311b8fc94bb5028e23e07ff367dad49
nickosp <nickospapageorge85@gmail.com>
2021-02-03 13:53:23 -0500

    finished random number with pc guessing

--- guess_number.py
+++ guess_number.py
@@ -14,3 +14,22 @@ def guess(x):
 
     print (f'Yay, you crushed it. The number is {random_number}. The Champ is here. The champ is you kid! ')            
 guess(10)
+
+def computer_guess(x):
+    low = 1
+    high = x
+    feedback = ''
+    while feedback != 'c': 
+        if low != high:
+            guess = random.randint(low, high)
+        else: 
+            guess = low #could also be high b/c low = high  
+        guess = random.randint(low, high)
+        feedback = input(f'Is {guess} too high (H), too low (L), or correct (C)?? ').lower()
+        if feedback == 'h':
+            high = guess - 1
+        elif feedback == 'l':
+            low = guess + 1
+        print(f'Yay the computer got it right {guess} and will start to takeover the world now!')  
+
+        computer_guess(1000)       
